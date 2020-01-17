e890fea748d38544ff5b41902ca01f7c09aedc2d
Garrett Brown <grbrown93@sbcglobal.net>
2020-01-17 01:00:28 -0500

    Add optional whitelist for added security

--- source/socket.cpp
+++ source/socket.cpp
@@ -2,8 +2,33 @@
 
 extern "C" int luaopen_socket_core( lua_State *state );
 
+int parseWhitelist();
+void clearWhitelist();
+enum : int
+{
+	PARSE_SUCCESS = 0,
+	PARSE_CANT_READ = 1,
+	PARSE_NO_ENTRIES = 2
+};
+
 GMOD_MODULE_OPEN( )
 {
+	#ifdef USE_WHITELIST
+		switch (parseWhitelist())
+		{
+		case PARSE_SUCCESS:
+			break;
+		case PARSE_CANT_READ:
+			LUA->ThrowError("Failed to read whitelist file!");
+			break;
+		case PARSE_NO_ENTRIES:
+			LUA->ThrowError("Didn't find any valid entries in whitelist file!");
+			break;
+		default:
+			break;
+		}
+	#endif
+
 	if( luaopen_socket_core( LUA->GetState( ) ) == 1 )
 	{
 		LUA->Push( -1 );
@@ -15,6 +40,10 @@ GMOD_MODULE_OPEN( )
 
 GMOD_MODULE_CLOSE( )
 {
+	#ifdef USE_WHITELIST
+		clearWhitelist();
+	#endif
+
 	LUA->PushNil( );
 	LUA->SetField( GarrysMod::Lua::INDEX_GLOBAL, "socket" );
 	return 0;
