8cc962a7ab6cbae6b49a601c1aee50984b1acc95
Daniel <danielga@outlook.pt>
2021-01-22 23:20:49 +0000

    Move every project into the same workspace

--- premake5.lua
+++ premake5.lua
@@ -13,27 +13,61 @@ include(gmcommon)
 local LUASOCKET_FOLDER = "luasocket/src"
 
 CreateWorkspace({name = "socket.core"})
-	CreateProject({serverside = true, manual_files = true})
-		defines("LUASOCKET_API=extern")
+	defines("LUASOCKET_API=extern")
+
+	filter("system:windows")
+		includedirs("include")
+
+	CreateProject({name = "socket.core", serverside = true, manual_files = true})
 		files("source/socket.cpp")
-		links("socket") -- socket needs to be linked before lua_shared
+		links("luasocket") -- luasocket needs to be linked before lua_shared
 		IncludeLuaShared()
 
 		filter("system:windows")
 			links("ws2_32")
 
-	CreateProject({serverside = false, manual_files = true})
-		defines("LUASOCKET_API=extern")
+	CreateProject({name = "socket.core", serverside = false, manual_files = true})
 		files("source/socket.cpp")
-		links("socket") -- socket needs to be linked before lua_shared
+		links("luasocket") -- luasocket needs to be linked before lua_shared
 		IncludeLuaShared()
 
 		filter("system:windows")
 			links("ws2_32")
 
-	project("socket")
+	CreateProject({name = "mime.core", serverside = true, manual_files = true})
+		files("source/mime.cpp")
+		links("luasocket") -- luasocket needs to be linked before lua_shared
+		IncludeLuaShared()
+
+	CreateProject({name = "mime.core", serverside = false, manual_files = true})
+		files("source/mime.cpp")
+		links("luasocket") -- luasocket needs to be linked before lua_shared
+		IncludeLuaShared()
+
+	CreateProject({name = "socket.unix", serverside = true, manual_files = true})
+		files("source/unix.cpp")
+		links("luasocket") -- luasocket needs to be linked before lua_shared
+		IncludeLuaShared()
+
+	CreateProject({name = "socket.unix", serverside = false, manual_files = true})
+		files("source/unix.cpp")
+		links("luasocket") -- luasocket needs to be linked before lua_shared
+		IncludeLuaShared()
+
+	if os.istarget("linux") or os.istarget("macosx") then
+		CreateProject({name = "socket.serial", serverside = true, manual_files = true})
+			files("source/serial.cpp")
+			links("luasocket") -- luasocket needs to be linked before lua_shared
+			IncludeLuaShared()
+
+		CreateProject({name = "socket.serial", serverside = false, manual_files = true})
+			files("source/serial.cpp")
+			links("luasocket") -- luasocket needs to be linked before lua_shared
+			IncludeLuaShared()
+	end
+
+	project("luasocket")
 		kind("StaticLib")
-		defines("LUASOCKET_API=extern")
 		includedirs({_GARRYSMOD_COMMON_DIRECTORY .. "/include", LUASOCKET_FOLDER})
 		files({
 			LUASOCKET_FOLDER .. "/auxiliar.c",
@@ -51,6 +85,8 @@ CreateWorkspace({name = "socket.core"})
 			LUASOCKET_FOLDER .. "/io.h",
 			LUASOCKET_FOLDER .. "/luasocket.c",
 			LUASOCKET_FOLDER .. "/luasocket.h",
+			LUASOCKET_FOLDER .. "/mime.c",
+			LUASOCKET_FOLDER .. "/mime.h",
 			LUASOCKET_FOLDER .. "/options.c",
 			LUASOCKET_FOLDER .. "/options.h",
 			LUASOCKET_FOLDER .. "/pierror.h",
@@ -61,90 +97,7 @@ CreateWorkspace({name = "socket.core"})
 			LUASOCKET_FOLDER .. "/timeout.c",
 			LUASOCKET_FOLDER .. "/timeout.h",
 			LUASOCKET_FOLDER .. "/udp.c",
-			LUASOCKET_FOLDER .. "/udp.h"
-		})
-		vpaths({
-			["Source files/*"] = LUASOCKET_FOLDER .. "/*.c",
-			["Header files/*"] = LUASOCKET_FOLDER .. "/*.h"
-		})
-		IncludeLuaShared()
-
-		filter("system:windows")
-			defines("_WINSOCK_DEPRECATED_NO_WARNINGS")
-			files({
-				LUASOCKET_FOLDER .. "/wsocket.c",
-				LUASOCKET_FOLDER .. "/wsocket.h"
-			})
-			links("ws2_32")
-
-		filter("system:not windows")
-			files({
-				LUASOCKET_FOLDER .. "/usocket.c",
-				LUASOCKET_FOLDER .. "/usocket.h"
-			})
-
-CreateWorkspace({name = "mime.core"})
-	CreateProject({serverside = true, manual_files = true})
-		defines("LUASOCKET_API=extern")
-		files("source/mime.cpp")
-		links("mime") -- mime needs to be linked before lua_shared
-		IncludeLuaShared()
-
-	CreateProject({serverside = false, manual_files = true})
-		defines("LUASOCKET_API=extern")
-		files("source/mime.cpp")
-		links("mime") -- mime needs to be linked before lua_shared
-		IncludeLuaShared()
-
-	project("mime")
-		kind("StaticLib")
-		defines("LUASOCKET_API=extern")
-		includedirs({_GARRYSMOD_COMMON_DIRECTORY .. "/include", LUASOCKET_FOLDER})
-		files({
-			LUASOCKET_FOLDER .. "/compat.c",
-			LUASOCKET_FOLDER .. "/compat.h",
-			LUASOCKET_FOLDER .. "/mime.c",
-			LUASOCKET_FOLDER .. "/mime.h"
-		})
-		vpaths({
-			["Source files/*"] = LUASOCKET_FOLDER .. "/*.c",
-			["Header files/*"] = LUASOCKET_FOLDER .. "/*.h"
-		})
-		IncludeLuaShared()
-
-CreateWorkspace({name = "socket.unix"})
-	CreateProject({serverside = true, manual_files = true})
-		defines("LUASOCKET_API=extern")
-		files("source/unix.cpp")
-		links("unix") -- unix needs to be linked before lua_shared
-		IncludeLuaShared()
-
-	CreateProject({serverside = false, manual_files = true})
-		defines("LUASOCKET_API=extern")
-		files("source/unix.cpp")
-		links("unix") -- unix needs to be linked before lua_shared
-		IncludeLuaShared()
-
-	project("unix")
-		kind("StaticLib")
-		defines("LUASOCKET_API=extern")
-		includedirs({_GARRYSMOD_COMMON_DIRECTORY .. "/include", LUASOCKET_FOLDER})
-		files({
-			LUASOCKET_FOLDER .. "/auxiliar.c",
-			LUASOCKET_FOLDER .. "/auxiliar.h",
-			LUASOCKET_FOLDER .. "/buffer.c",
-			LUASOCKET_FOLDER .. "/buffer.h",
-			LUASOCKET_FOLDER .. "/compat.c",
-			LUASOCKET_FOLDER .. "/compat.h",
-			LUASOCKET_FOLDER .. "/inet.c",
-			LUASOCKET_FOLDER .. "/inet.h",
-			LUASOCKET_FOLDER .. "/io.c",
-			LUASOCKET_FOLDER .. "/io.h",
-			LUASOCKET_FOLDER .. "/options.c",
-			LUASOCKET_FOLDER .. "/options.h",
-			LUASOCKET_FOLDER .. "/pierror.h",
-			LUASOCKET_FOLDER .. "/timeout.c",
-			LUASOCKET_FOLDER .. "/timeout.h",
+			LUASOCKET_FOLDER .. "/udp.h",
 			LUASOCKET_FOLDER .. "/unix.c",
 			LUASOCKET_FOLDER .. "/unix.h",
 			LUASOCKET_FOLDER .. "/unixdgram.c",
@@ -160,7 +113,6 @@ CreateWorkspace({name = "socket.unix"})
 
 		filter("system:windows")
 			defines("_WINSOCK_DEPRECATED_NO_WARNINGS")
-			includedirs("include")
 			files({
 				LUASOCKET_FOLDER .. "/wsocket.c",
 				LUASOCKET_FOLDER .. "/wsocket.h"
@@ -169,49 +121,7 @@ CreateWorkspace({name = "socket.unix"})
 
 		filter("system:not windows")
 			files({
+				LUASOCKET_FOLDER .. "/serial.c",
 				LUASOCKET_FOLDER .. "/usocket.c",
 				LUASOCKET_FOLDER .. "/usocket.h"
 			})
-
-if os.istarget("linux") or os.istarget("macosx") then
-	CreateWorkspace({name = "socket.serial"})
-		CreateProject({serverside = true, manual_files = true})
-			defines("LUASOCKET_API=extern")
-			files("source/serial.cpp")
-			links("serial") -- serial needs to be linked before lua_shared
-			IncludeLuaShared()
-
-		CreateProject({serverside = false, manual_files = true})
-			defines("LUASOCKET_API=extern")
-			files("source/serial.cpp")
-			links("serial") -- serial needs to be linked before lua_shared
-			IncludeLuaShared()
-
-		project("serial")
-			kind("StaticLib")
-			defines("LUASOCKET_API=extern")
-			includedirs({_GARRYSMOD_COMMON_DIRECTORY .. "/include", LUASOCKET_FOLDER})
-			files({
-				LUASOCKET_FOLDER .. "/auxiliar.c",
-				LUASOCKET_FOLDER .. "/auxiliar.h",
-				LUASOCKET_FOLDER .. "/buffer.c",
-				LUASOCKET_FOLDER .. "/buffer.h",
-				LUASOCKET_FOLDER .. "/compat.c",
-				LUASOCKET_FOLDER .. "/compat.h",
-				LUASOCKET_FOLDER .. "/io.c",
-				LUASOCKET_FOLDER .. "/io.h",
-				LUASOCKET_FOLDER .. "/options.c",
-				LUASOCKET_FOLDER .. "/options.h",
-				LUASOCKET_FOLDER .. "/pierror.h",
-				LUASOCKET_FOLDER .. "/timeout.c",
-				LUASOCKET_FOLDER .. "/timeout.h",
-				LUASOCKET_FOLDER .. "/usocket.c",
-				LUASOCKET_FOLDER .. "/usocket.h",
-				LUASOCKET_FOLDER .. "/serial.c"
-			})
-			vpaths({
-				["Source files/*"] = LUASOCKET_FOLDER .. "/*.c",
-				["Header files/*"] = LUASOCKET_FOLDER .. "/*.h"
-			})
-			IncludeLuaShared()
-end
